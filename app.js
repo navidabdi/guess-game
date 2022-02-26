9de6267fa6040a0b1ce2d311837903511a6662f7
navidabdi <artsanaat@gmail.com>
2022-02-26 09:01:20 +0330

    change the algorithm

--- app.js
+++ app.js
@@ -3,51 +3,50 @@
 // If they guess and they are wrong, asks them again (Hint)
 // If they win - say that they won
 
-function guessGame(){
+// function guessGame(){
 
-    let randNumber = Math.floor(Math.random() * 6)+1;
-    let userGuess = document.getElementById('userGuess').value;
+//     let randNumber = Math.floor(Math.random() * 6)+1;
+//     let userGuess = document.getElementById('userGuess').value;
 
-    let userGuessDom = document.querySelector('.user-guess');
-    let gameNumDom = document.querySelector('.game-nubmer');
+//     let userGuessDom = document.querySelector('.user-guess');
+//     let gameNumDom = document.querySelector('.game-nubmer');
 
-    let showWonOrLose = document.querySelector('.won-lose h2');
+//     let showWonOrLose = document.querySelector('.won-lose h2');
 
-    let totalWonDom = document.querySelector('.totalWonDom');
-    let totalLoseDom = document.querySelector('.totalLoseDom');
+//     let totalWonDom = document.querySelector('.totalWonDom');
+//     let totalLoseDom = document.querySelector('.totalLoseDom');
 
+//     let totalWon = 0;
+//     let totalLose= 0;
+//     let totalGames =0;
 
-    let totalWon = 0;
-    let totalLose= 0;
-    let totalGames =0;
+//     if(userGuess < 1 || userGuess > 6){
+//         showWonOrLose.classList.remove('won');
+//         showWonOrLose.innerHTML = 'Choose Between 1 - 6';
+//     }else {
 
-    if(userGuess < 1 || userGuess > 6){
-        showWonOrLose.classList.remove('won');
-        showWonOrLose.innerHTML = 'Choose Between 1 - 6';
-    }else {
+//         if(randNumber == userGuess){
+//             totalWon++;
+//             totalWonDom.innerHTML = totalWon;
 
-        if(randNumber == userGuess){
-            totalWon++;
-            totalWonDom.innerHTML = totalWon;
+//             showWonOrLose.classList.add('won');
+//             showWonOrLose.innerHTML = 'You Are Won!';
+//         }else{
+//             totalLose++;
 
-            showWonOrLose.classList.add('won');
-            showWonOrLose.innerHTML = 'You Are Won!';
-        }else{
-            totalLose++;
-            
-            totalLoseDom.innerHTML = totalLose;
-            showWonOrLose.classList.remove('won');
-            showWonOrLose.innerHTML = 'You Are Loose!';
-        }
+//             totalLoseDom.innerHTML = totalLose;
+//             showWonOrLose.classList.remove('won');
+//             showWonOrLose.innerHTML = 'You Are Loose!';
+//         }
 
-        userGuessDom.innerHTML = userGuess;
-        gameNumDom.innerHTML = randNumber; 
-        
-    }
+//         userGuessDom.innerHTML = userGuess;
+//         gameNumDom.innerHTML = randNumber;
 
-} 
+//     }
 
-let btn = document.querySelector('.sumbit-guess');
-btn.addEventListener('click',()=>{
-    guessGame();    
-})
+// }
+
+// let btn = document.querySelector('.sumbit-guess');
+// btn.addEventListener('click',()=>{
+//     guessGame();
+// })
